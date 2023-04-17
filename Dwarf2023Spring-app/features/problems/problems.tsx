884911900826ed53bd675e96d764da19b770619f
Christian Kirschberg <ckirschberg@gmail.com>
2023-04-17 13:16:19 +0200

    .

--- Dwarf2023Spring-app/features/problems/problems.tsx
+++ Dwarf2023Spring-app/features/problems/problems.tsx
@@ -3,55 +3,92 @@ import { AppDispatch, RootState } from '../../store'
 import { useSelector, useDispatch } from 'react-redux'
 import { createProblem, fetchAllProblems } from './problemsSlice'
 import { ProblemEntity } from './problemEntity'
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button, Image } from 'react-native'
+import { Picture } from './picture'
+import * as ImagePicker from 'expo-image-picker';
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
+import { MediaType } from 'expo-media-library'
+
+
 
 export function Problems() {
   const count = useSelector((state: RootState) => state.counter.value)
   const problems: ProblemEntity[] = useSelector((state: RootState) => state.problems.problems)
-  
-  const dispatch = useDispatch<AppDispatch>()
+  const [camera, setCamera] = useState(false);
 
+  const dispatch = useDispatch<AppDispatch>()
   const [subject, setSubject] = useState('');
   const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+
+  const [photoToDisplay, setPhotoToDisplay] = useState('')
+  // console.log("photoToDisplay", photoToDisplay);
+  
+
+  useEffect(() => {
+    (async () => {
+      
+    })();
+  }, []);
+
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
     console.log(`subject: ${subject}, description: ${description}`);
 
-    dispatch(createProblem(new ProblemEntity(subject, description)));
+    dispatch(createProblem(new ProblemEntity(subject, description, photoToDisplay)));
   }
 
   useEffect(() => {
-    dispatch(fetchAllProblems())
+    // dispatch(fetchAllProblems())
   }, [])
 
   return (
-    <View>
-        <View>
-            <TextInput
-                style={styles.input}
-                onChangeText={setSubject}
-                value={subject}
-            />
-            <TextInput
-                style={styles.input}
-                onChangeText={setDescription}
-                value={description}
-            />
-            <Button title="Create problem" onPress={handleSubmit}/>
-        </View>
+    
+    <View style={{flex:1}}>
+      {camera ? <Picture setCamera={setCamera} setPhotoToDisplay={setPhotoToDisplay}></Picture> : <>
+          <TextInput
+              style={styles.input}
+              onChangeText={setSubject}
+              value={subject}
+          />
+          <TextInput
+              style={styles.input}
+              onChangeText={setDescription}
+              value={description}
+          />
+          
+          <View style={styles.container}>
+            {image && <Image source={{ uri: image }} style={styles.image} />}
+            {!image && <Text>No image selected</Text>}
+          </View>
+        
+        <Button title="Open camera" onPress={() => setCamera(true)}/>
+        <Button title="Create problem" onPress={handleSubmit}/>
+        </>}
 
-        {problems.map((problem) => (
+    </View>
+
+       /* {problems.map((problem) => (
             <View key={problem?.id}>
                 <Text>{problem?.subject} - {problem?.description}</Text>
             </View>
-        ))}
-    </View>
+        ))} */
   );
 }
 
 const styles = StyleSheet.create({
-    input: {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  input: {
       height: 40,
       margin: 12,
       borderWidth: 1,
